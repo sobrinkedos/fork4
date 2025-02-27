7ad595668eb343ceeeaf3b1726913df3ac2e3318
riltons <riltons@gmail.com>
2025-02-26 22:50:47 -0300

    Correção da rota do botão FAB na página de jogadores
    - Atualizada a rota para o botão de adicionar novo jogador de '/jogador/novo' para '/jogador/jogador/novo'.
    - Isso garante que a página de criação de jogador seja carregada corretamente.

--- src/app/(tabs)/jogadores.tsx
+++ src/app/(tabs)/jogadores.tsx
@@ -351,7 +351,7 @@ export default function Jogadores() {
                     }
                 />
 
-                <FAB onPress={() => router.push('/jogador/novo')}>
+                <FAB onPress={() => router.push('/jogador/jogador/novo')}>
                     <Feather name="plus" size={24} color={colors.backgroundLight} />
                 </FAB>
             </Content>
